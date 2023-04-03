c5f1e5da1744ab0080d80bec598221f4469b2484
Philip Kumah <philip.kumah@qubole.com>
2023-04-03 19:16:50 +0000

    New commit for calculator

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter operation (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        double result = 0.0;
+
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                break;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    }
+}
+
